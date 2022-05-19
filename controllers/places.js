3a1842a61c1b8ef134a85b9e6f20b32cdf1be93f
Joel Aguirre <joelaguirre@Joels-MacBook-Pro.local>
2022-05-18 22:02:17 -0500

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,18 +1,7 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
-let places = [{
-    name: 'Mexican Food',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Mexican Cuisine',
-    pic: '/images/mexican-restaurant.jpeg'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/cafecafe.jpeg'
-  }]
+
 
   router.get('/new', (req, res) => {
     res.render('places/new')
@@ -26,8 +15,20 @@ let places = [{
 
 router.post('/', (req, res) => {
   console.log(req.body)
-  res.send('POST /places')
+  if (!req.body.pic) {
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
+    
+
 
 
 module.exports = router
---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'Mexican Food',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Mexican Cuisine',
+    pic: '/images/mexican-restaurant.jpeg'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/cafecafe.jpeg'
+  }]
