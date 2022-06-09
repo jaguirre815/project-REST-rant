2b8a5ffe2fd0bad7f8184d73b5292c1018ed723a
Joel Aguirre <joelaguirre@Joels-MacBook-Pro.local>
2022-06-08 22:36:23 -0500

    bootstrap link fixed

--- views/default.jsx
+++ views/default.jsx
@@ -6,8 +6,8 @@ function Def (html) {
     return (
         <html>
             <head>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossOrigin="anonymous" />
-                <link rel="stylesheet" href="/style.css" />
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"></link>
+                <link rel="stylesheet" href="public/css/style.css" />
                 <title>Title</title>
             </head>
             <body>
